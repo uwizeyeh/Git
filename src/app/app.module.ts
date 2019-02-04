91da1f5289fefd08be50421de83f32766ca97bb2
uwizeyeh <uwizeyimanahulde1@gmail.com>
2019-02-04 12:56:13 +0200

    add folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
+    // FormModule,
     AppRoutingModule
   ],
   providers: [],
